9b9c45dbcab4afa7a072b48baed52ed0532a53a2
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2024-12-28 22:50:54 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplicacao(20, 10));
+function dizOla(nome) {
+    const ola = 'Olá ';
+    return ola + nome;
+}
+console.log(dizOla('Marcos'));
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+function multiplicacao (numero1: number, numero2: number):number {
+
+    return numero1 * numero2;
+
+}
+
+console.log(multiplicacao(20, 10));
+
+
+function dizOla (nome: string): string {
+
+    const ola: string = 'Olá '
+
+    return ola + nome;
+
+}
+
+console.log(dizOla('Marcos'));
+
